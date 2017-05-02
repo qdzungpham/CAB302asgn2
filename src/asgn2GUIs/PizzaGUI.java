3dbc09cfef807d85b7a9cbdd449bf47f2a931750
Rick Pham <q3.pham@connect.qut.edu.au>
2017-05-02 16:57:36 +1000

    Implemented GUI

--- src/asgn2GUIs/PizzaGUI.java
+++ src/asgn2GUIs/PizzaGUI.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.text.DecimalFormat;
 
 import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
 import javax.swing.text.DefaultCaret;
 
 import asgn2Customers.Customer;
@@ -37,18 +38,78 @@ public class PizzaGUI extends javax.swing.JFrame implements Runnable, ActionList
 	
 	private PizzaRestaurant restaurant;
 	
+	private static PizzaGUI gui;
+	private JButton browseFileBtn;
+	private JButton displayInfoBtn;
+	private JTable customersTable;
+	private JTable pizzasTable;
+	
 	/**
 	 * Creates a new Pizza GUI with the specified title 
 	 * @param title - The title for the supertype JFrame
 	 */
 	public PizzaGUI(String title) {
 		// TO DO
+		super(title);
+		initComponents();
+		gui = this;
 	}
 
+	private void initComponents() {
+		JPanel filePanel = new JPanel();
+		JLabel fileLabel = new JLabel("Select a file: ");
+		JTextField filePath = new JTextField(30);
+		browseFileBtn = new JButton("Browse...");
+		displayInfoBtn = new JButton("Display the information");
+		filePanel.add(fileLabel);
+		filePanel.add(filePath);
+		filePanel.add(browseFileBtn);
+		filePanel.add(displayInfoBtn);
+		
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BorderLayout());
+		
+		JPanel customersPanel = new JPanel();
+		customersPanel.setBorder(BorderFactory.createTitledBorder("Customers"));
+		customersTable = new JTable();
+		JScrollPane customersScroller = new JScrollPane(customersTable);
+		customersScroller.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		customersPanel.add(customersScroller);
+		
+		JPanel pizzasPanel = new JPanel();
+		pizzasPanel.setBorder(BorderFactory.createTitledBorder("Pizzas"));
+		pizzasTable = new JTable();
+		JScrollPane pizzasScroller = new JScrollPane(pizzasTable);
+		pizzasScroller.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		pizzasPanel.add(pizzasScroller);
+		
+		mainPanel.add(filePanel, BorderLayout.NORTH);
+		mainPanel.add(customersPanel, BorderLayout.CENTER);
+		mainPanel.add(pizzasPanel, BorderLayout.SOUTH);
+		
+		getContentPane().add(mainPanel);
+	}
 	
 	@Override
 	public void run() {
 		// TO DO
+		createAndShowGUI();
+	}
+	
+	private static void createAndShowGUI() {
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		
+		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//gui.setPreferredSize(new Dimension(500, 200));
+		gui.setLocation(new Point(200, 200));
+		gui.pack();
+		gui.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	
